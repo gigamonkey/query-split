8555d0e4d10770760aa0a122004cf5734315ee04
Peter Seibel <pseibel@twitter.com>
2013-09-15 12:42:33 -0700

    Simplifying simplifying code.

--- query-split.lisp
+++ query-split.lisp
@@ -53,212 +53,49 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Boolean expression simplification. We can simplify boolean
-;;; expressions in a number of ways, some of which will be
-;;; particularly useful for the expressions that come out of bias-to
-;;; since we are likely to end up with expressions with more than the
-;;; usual number of literals which can make more complex expressions
-;;; take on known values. (E.g. (or v1 t) => t)
-;;;
-;;; Basic strategy: first flatten out the expression so that things
-;;; like (and a (and b (and c))) turn into (and a b c). (We can also
-;;; be a bit clever with De Morgan's Laws to flatten (and a (not (or b
-;;; c))) into (and a (not b) (not c)).
-;;;
-;;; Now we really go to town, working from the bottom up. First
-;;; simplify any ands containing literal nils, ors containing literal
-;;; ts and nots containing any literal. In the same pass, we can
-;;; probably eliminate any literal ts from ands and literal nils from
-;;; ors. And eliminate duplicates sub-expressions from either. And if
-;;; after all that we end up with empty ands or ors they can be
-;;; replaced with their identity value.
-;;;
-;;; Perhaps at the same time
-
-;;; (and v1 ... complex expressions eventually using v1 ...)
-;;;
-;;; Can still work bottom up, simplifying any ands and ors deep down
-;;; in the expression first, then as we work our way back up, we may
-;;; reach down into those expressions to change them based on
-;;; higher-level assumed values. We could either re-simplify any
-;;; expression after it is changed by rewriting or we could apply all
-;;; rewriting all the way up to the top and then resimplify the whole
-;;; tree. Probably better to do it on the way up since the worst that
-;;; can happen is a low-level simplification would remove some
-;;; expression which a higher-level rewrite might have rewritten.
-;;;
-;;; Any rewrites done at a low-level that lead to simplifiable
-;;; expressions, will still be there after doing higher-level
-;;; rewrites. So might as well do the rewrites bottom up and then do
-;;; another, single, bottom-up simplification pass to simplify
-;;; anything that came up during the rewrites.
-;;;
-;;; So: after flattening and doing one bottom-up simplification, do a
-;;; bottom up, rewrite and simplify pass.
-
-;;;
-;;; Next, we can try to simplify expressions by replacing
-;;; sub-expressions with the values they must have in order for
-;;; enclosing expressions to have the value they must have. To take a
-;;; simple example, if our whole expression was (and a (or a b) we can
-;;; say, well, if a is nil the whole expression will be nil regardless
-;;; of what the value of (or a b) is. Therefore we can rewrite (or a
-;;; b) under the assumption that a is true. Thus (and a (or a b)) =>
-;;; (and a (or t b)) which we can later simplify to (and a t) and
-;;; finally to a. Which is kind of interesting in that the value of b
-;;; has gone away entirely but that's actually logically correct. This
-;;; step, however, could lead to an exponential explosion in the
-;;; amount of work.
-
-;;; sub-expressions. For example In
-;;; any AND expression we can take each operand in turn and assume
-;;; that it is true and determine any other expressions that will have
-;;; known values under that assumption. Then see if any of those
-;;; expressions occur in any of the other operands and replace them
-;;; with the literal value. (This is legit because if we have (and a
-;;; b), it can only be true if a is true. Therefore we can assume a is
-;;; true and replace any occurence of it in b with t. If a is not true
-;;; then the and will evaluate to false and it doesn't matter if
-;;; changing a to t has made the value of b change. And if a is true,
-;;; then replacing it with t is totally legit. And likewise with b
-;;; since (and a b) === (and b a). This will handle the trivial case
-;;; of (and a (not a)) by transforming it to (and a (not t)) => (and a
-;;; nil) which will get turned into nil in the next step.
-;;;
-;;;
-;;; Finally, remove any literal t's from and expressions and any
-;;; literal nil's from or expressions and convert empty ands to t and
-;;; or to nil. Again, this should probably be done from the bottom up.
+;;; Expression simplification.
 
 (defun simplify (exp)
   (typecase exp
-    (symbol
-     exp)
+    (symbol exp)
     (cons
-     (destructuring-bind (operator . args) exp
-       (let ((simple-args (mapcar #'simplify args)))
-         (case operator
-           (and (simplify-and/or operator simple-args t))
-           (or (simplify-and/or operator simple-args nil))
-           (not (simplify-not simple-args))))))))
+     (case (car exp)
+       (and (simplify-and/or exp t))
+       (or (simplify-and/or exp nil))
+       (not (simplify-not exp))))))
 
-(defun simplify-and (exp)
+(defun simplify-and/or (exp id)
   (destructuring-bind (op arg1 arg2) exp
-    (assert (eql op 'and))
-    (if (equal arg1 arg2)
-        arg1
-        (let ((arg1 (maybe-rewrite arg1 (must-be t arg2)))
-              (arg2 (maybe-rewrite arg2 (must-be t arg1))))
-          (cond
-            ((or (eql arg1 nil) (eql arg2 nil)) nil)
-            ((eql arg1 t) arg2)
-            ((eql arg2 t) arg1)
-            ((equal arg1 arg2) arg1)
-            ((equal arg1 `(not ,arg2)) nil)
-            ((equal arg2 `(not ,arg1)) nil)
-            (t `(,op ,arg1 ,arg2)))))))
+    (let ((arg1 (simplify arg1))
+          (arg2 (simplify arg2)))
+      ;; need to check this before rewriting lest we rewrite,
+      ;; e.g. (and a a) => (and t t) => t
+      (if (equal arg1 arg2)
+          arg1
+          (let ((arg1 (maybe-rewrite arg1 (must-be id arg2)))
+                (arg2 (maybe-rewrite arg2 (must-be id arg1))))
+            (cond
+              ((or (eql arg1 (not id)) (eql arg2 (not id))) (not id))
+              ((eql arg1 id) arg2)
+              ((eql arg2 id) arg1)
+              ((equal arg1 arg2) arg1)
+              ((equal arg1 `(not ,arg2)) (not id))
+              ((equal arg2 `(not ,arg1)) (not id))
+              (t `(,op ,arg1 ,arg2))))))))
 
-(defun simplify-or (exp)
-  (destructuring-bind (op arg1 arg2) exp
-    (assert (eql op 'or))
-    (if (equal arg1 arg2) ;; need to check this before rewriting
-        arg1
-        (let ((arg1 (maybe-rewrite arg1 (must-be nil arg2)))
-              (arg2 (maybe-rewrite arg2 (must-be nil arg1))))
-          (cond
-            ((or (eql arg1 t) (eql arg2 t)) t)
-            ((eql arg1 nil) arg2)
-            ((eql arg2 nil) arg1)
-            ((equal arg1 arg2) arg1)
-            ((equal arg1 `(not ,arg2)) t)
-            ((equal arg2 `(not ,arg1)) t)
-            (t `(,op ,arg1 ,arg2)))))))
-
+(defun simplify-not (exp)
+  (destructuring-bind (op arg) exp
+    (assert (eql op 'not))
+    (let ((arg (simplify arg)))
+      (cond
+        ((eql arg t) nil)
+        ((eql arg nil) t)
+        ((and (consp arg) (eql (car arg) 'not)) (cadr arg))
+        (t `(not ,arg))))))
 
 (defun maybe-rewrite (exp must-be-table)
   (let ((new (sublis must-be-table exp)))
     (if (not (equal new exp)) (simplify new) exp)))
-
-
-(defun simplify-and/or (op simple-args identity)
-  (let ((args (remove-duplicates (remove identity simple-args) :test #'equal)))
-    (cond
-      ;; no interesting args, identity for this operator
-      ((not args) identity)
-
-      ;; (and ... nil ...) is nil and (or ... t ...) is t
-      ((member (not identity) args) (not identity))
-
-      ;; Only one distinct arg, it's the value
-      ((only-one args) (first args))
-
-      ;; !a & a => nil; !a | a => t
-      ((opposite-p (first args) (second args)) (not identity))
-
-
-      ((and (symbolp (first args)) (consp (second args)))
-       (let* ((sym (first args))
-              (other (second args))
-              (other-simplified (simplify (subst identity sym other))))
-         (if (equal other other-simplified)
-             `(,op ,sym ,other)
-             (simplify `(,op ,sym ,other-simplified)))))
-      ((and (symbolp (second args)) (consp (first args)))
-       (let* ((sym (second args))
-              (other (first args))
-              (other-simplified (simplify (subst identity sym other))))
-         (if (equal other other-simplified)
-             `(,op ,other ,sym)
-             (simplify `(,op ,other-simplified ,sym)))))
-      (t `(,op ,@args)))))
-
-(defun simplify-not (simple-args)
-  (destructuring-bind (arg) simple-args
-    (cond
-      ((eql arg t) nil)
-      ((eql arg nil) t)
-      ((not-expression-p arg) (second arg))
-      (t `(not ,arg)))))
-
-(defun only-one (list) (not (rest list)))
-
-(defun not-expression-p (x) (and (consp x) (eql (first x) 'not)))
-
-(defun opposite-p (arg1 arg2)
-  (or (and (not-expression-p arg1) (equal (second arg1) arg2))
-      (and (not-expression-p arg2) (equal (second arg2) arg1))))
-
-
-
-(defun flatten (exp)
-  "Flatten expression so binary and's and or's are turned into multi-arg versions."
-  (typecase exp
-    (symbol exp)
-    (cons
-     (destructuring-bind (op . args) exp
-       (case op
-         ((and or)
-          `(,op ,@(mapcan #'(lambda (x) (flat-terms op x)) (mapcar #'flatten args))))
-         (not `(not ,(flatten (first args)))))))))
-
-(defun flat-terms (op exp)
-  "Get the list of terms we can extract from an expression, assuming
-we're in the context of op."
-  (typecase exp
-    (symbol (list exp))
-    (cons
-     (cond
-       ((eql op (car exp)) (rest exp))
-       ((de-morgan-p op exp) (mapcar #'(lambda (x) `(not ,x)) (cdadr exp)))
-       (t (list exp))))))
-
-(defun de-morgan-p (op exp)
-  (and (consp exp)
-       (eql 'not (car exp))
-       (consp (cadr exp))
-       (eql (case op (and 'or) (or 'and)) (caadr exp))))
-
 
 (defun must-be (value expr)
   "Assuming that expr must evaluate to the given value, return an
@@ -277,7 +114,6 @@ we're in the context of op."
          (unless value
            (mapcan #'(lambda (x) (must-be nil x)) (cdr expr))))
         (not (must-be (not value) (cadr expr))))))))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Generating random expressions
@@ -335,7 +171,7 @@ we're in the context of op."
 ;;; return false.) Then from the unique permutations of the cheap
 ;;; variables, see for how many g(x) returns false.
 
-(defparameter *verbose-check* nill)
+(defparameter *verbose-check* nil)
 
 (defun test (input-line)
   (let* ((full-expression (parse input-line))
